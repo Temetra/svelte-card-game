61bc306990d58b6a64c642f1f0fec8315ea8983c
Temetra <temetra.xy+git@gmail.com>
2020-08-24 17:55:55 +0100

    Trying rollup-plugin-serve

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import serve from "rollup-plugin-serve";
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
@@ -13,27 +14,6 @@ import alias from "@rollup/plugin-alias";
 import babel from "@rollup/plugin-babel";
 import scss from "rollup-plugin-scss";
 import svelteSVG from "rollup-plugin-svelte-svg";
-
-function serve() {
-	let server;
-	
-	function toExit() {
-		if (server) server.kill(0);
-	}
-
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev', '--host'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
 
 export default {
 	input: 'src/main.ts',
@@ -95,7 +75,12 @@ export default {
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
-		!production && serve(),
+		!production && serve({
+			open: false,
+			contentBase: "public",
+			host: "0.0.0.0",
+			port: 5000
+		}),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
