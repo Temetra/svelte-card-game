a309a8e8159d73a3fd5b9a542e63cafcb1f3d28d
Temetra <temetra.xy+git@gmail.com>
2020-09-01 18:12:28 +0100

    Added Jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	"roots": [
+		"<rootDir>/src"
+	],
+	"testMatch": [
+		"**/__tests__/**/*.+(spec|test).+(ts|tsx|js)"
+	],
+	"transform": {
+		"^.+\\.svelte$": ["svelte-jester", { "preprocess": true }],
+		"^.+\\.(ts|tsx)$": "ts-jest",
+	},
+	"moduleNameMapper": {
+		'^@/(.*)$': '<rootDir>/src/$1',
+	},
+	"moduleFileExtensions": [
+		"js",
+		"ts",
+		"svelte",
+	],
+}
