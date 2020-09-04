a450e296d104110b8f5d64d0d8d9b200c7caa246
Temetra <temetra.xy+git@gmail.com>
2020-09-04 06:26:20 +0100

    Replaced requestNewCards tests with snapshot

--- src/__tests__/Game/RequestNewCards.test.ts
+++ src/__tests__/Game/RequestNewCards.test.ts
@@ -3,62 +3,30 @@ import { GameState, gameState, playerOneHand, requestNewCards } from "@/modules/
 import { CardState } from "@/modules/Cards";
 export{}
 
-let states: GameState[] = [];
+let states: string[] = [];
 let unsubGameState;
 
-let hands: CardState[][] = [];
+let hands: string[][] = [];
 let unsubHands;
 
 beforeEach(() => {
 	if (unsubGameState) unsubGameState();
 	gameState.set(GameState.Preparing);
 	states = [];
-	unsubGameState = gameState.subscribe(x => states.push(x));
+	unsubGameState = gameState.subscribe(x => states.push(GameState[x]));
 
 	if (unsubHands) unsubHands();
 	playerOneHand.set([]);
 	hands = [];
-	unsubHands = playerOneHand.subscribe(x => hands.push(x.map(y => y.state)));
+	unsubHands = playerOneHand.subscribe(x => hands.push(x.map(y => CardState[y.state])));
 });
 
 test("requestNewCards", async () => {
-	expect(states).toStrictEqual([GameState.Preparing]);
+	expect(states).toStrictEqual(["Preparing"]);
 	expect(hands).toStrictEqual([[]]);
 	
 	await requestNewCards();
 	
-	expect(states).toStrictEqual([
-		GameState.Preparing,
-		GameState.Dealing,
-		GameState.Selecting
-	]);
-
-	// dealAllCards, cards are hidden
-	let hand = hands[2];
-	expect(hand[0] === CardState.Hidden).toBeTruthy();
-	expect(hand[4] === CardState.Hidden).toBeTruthy();
-
-	// animateDealtCard #1, first card is dealing
-	hand = hands[3];
-	expect(hand[0] === CardState.Dealing).toBeTruthy();
-	expect(hand[2] === CardState.Hidden).toBeTruthy();
-	expect(hand[4] === CardState.Hidden).toBeTruthy();
-
-	// animateDealtCard #3, 3rd card is dealing
-	hand = hands[5];
-	expect(hand[0] === CardState.Dealing).toBeTruthy();
-	expect(hand[2] === CardState.Dealing).toBeTruthy();
-	expect(hand[4] === CardState.Hidden).toBeTruthy();
-
-	// animateDealtCard #5, last card is dealing
-	hand = hands[7];
-	expect(hand[0] === CardState.Dealing).toBeTruthy();
-	expect(hand[2] === CardState.Dealing).toBeTruthy();
-	expect(hand[4] === CardState.Dealing).toBeTruthy();
-
-	// finishDealing
-	hand = hands[8];
-	expect(hand[0] === 0).toBeTruthy();
-	expect(hand[2] === 0).toBeTruthy();
-	expect(hand[4] === 0).toBeTruthy();
+	expect(states).toMatchSnapshot();
+	expect(hands).toMatchSnapshot();
 });
