bf430b968454218a6c57048175439235bfa44287
Temetra <temetra.xy+git@gmail.com>
2020-09-05 09:54:33 +0100

    CardGraphic snapshot for face only

--- src/__tests__/Components/CardGraphic.test.ts
+++ src/__tests__/Components/CardGraphic.test.ts
@@ -20,10 +20,13 @@ test("CardGraphic", async () => {
 	expect(component).not.toBeNull();
 
 	// Run tests
-	let graphic = document.body.getElementsByTagName("section");
-	expect(graphic.length).toBe(1);
-	expect(graphic[0].classList.length).toBe(1);
-	expect(graphic[0]).toMatchSnapshot();
+	let section = document.body.getElementsByTagName("section").item(0);
+	expect(section).not.toBeNull();
+	expect(section.classList.length).toBe(1);
+
+	let graphic = section.getElementsByClassName("face").item(0);
+	expect(graphic).not.toBeNull();
+	expect(graphic).toMatchSnapshot();
 });
 
 test("flipped focused", async () => {
@@ -35,9 +38,9 @@ test("flipped focused", async () => {
 	expect(component).not.toBeNull();
 
 	// Run tests
-	let graphic = document.body.getElementsByTagName("section");
-	expect(graphic.length).toBe(1);
-	expect(graphic[0].classList.length).toBe(3);
-	expect(graphic[0].classList.contains("flipped")).toBeTruthy();
-	expect(graphic[0].classList.contains("focused")).toBeTruthy();
+	let section = document.body.getElementsByTagName("section").item(0);
+	expect(section).not.toBeNull();
+	expect(section.classList.length).toBe(3);
+	expect(section.classList.contains("flipped")).toBeTruthy();
+	expect(section.classList.contains("focused")).toBeTruthy();
 });
