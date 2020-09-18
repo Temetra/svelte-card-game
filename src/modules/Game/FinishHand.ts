d2b50de1e90921f0520084d2b53917854e14282a
Temetra <temetra.xy+git@gmail.com>
2020-09-18 12:07:58 +0100

    Prevent score from updating when finishing

--- src/modules/Game/FinishHand.ts
+++ src/modules/Game/FinishHand.ts
@@ -3,6 +3,7 @@ import { GameState, gameState, playerOneHand } from "@/modules/Game/GameState";
 // This is a player-triggered action
 // Clears the hand and sets ready state for a new game
 export function finishHand() {
+	gameState.set(GameState.Clearing);
 	playerOneHand.set([]);
 	gameState.set(GameState.Ready);
 }
